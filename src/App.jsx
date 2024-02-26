8d1168fc9983607f5fabcaaa7bcd384f08e15f21
zeenat <zeenu.23@gmail.com>
2024-02-26 16:16:31 +0530

    add AppLayout component as the root element for all routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,13 @@ import Menu from './features/menu/Menu'
 import Cart from './features/cart/Cart'
 import CreateOrder from './features/order/CreateOrder'
 import Order from './features/order/Order'
+import AppLayout from './ui/AppLayout'
 
 const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout/>,
+    children: [
   {
     path: "/",
     element: <Home />,
@@ -27,7 +32,9 @@ const router = createBrowserRouter([
     path: "/order/:orderId",
     element: <Order/>,
   }
-])
+    ]
+  }
+ ])
 
 function App() {
   return <RouterProvider router={router} />
