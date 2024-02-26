2373f315a04034d61c3e2dcc7b2881c4c2483a43
zeenat <zeenu.23@gmail.com>
2024-02-26 15:16:01 +0530

    Add react-router-dom and update package.json and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { createBrowserRouter } from 'react-router-dom'
 import './index.css'
 
+const router = createBrowserRouter([
+   {
+    path: "/",
+    element: <Root />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "team",
+        element: <Team />,
+        loader: teamLoader,
+      },
+    ],
+  },
+])
 function App() {
   return (
     <div>
