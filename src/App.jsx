3eb3ad4b28dd40db089cc8e3cf743aa794d1a8fa
zeenat <zeenu.23@gmail.com>
2024-02-26 15:31:11 +0530

    add Cart page to the router

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
 import Home from './ui/Home'
 import Menu from './features/menu/Menu'
+import Cart from './features/cart/Cart'
 
 const router = createBrowserRouter([
-   {
+  {
     path: "/",
     element: <Home />,
   },
-   {
+  {
     path: "/menu",
     element: <Menu />,
   },
+  {
+    path: "/cart",
+    element: <Cart/>,
+  }
 ])
 
 function App() {
