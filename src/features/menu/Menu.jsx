b28892c3040d7546e433d2402d4d8cb80adb0baa
zeenat <zeenu.23@gmail.com>
2024-02-28 22:38:53 +0530

    Add `useLoaderData` and `console.log` to the `Menu` component

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,6 +1,9 @@
+import { useLoaderData } from 'react-router-dom';
 import { getMenu } from '../../services/apiRestaurant'
 
 function Menu() {
+  const menu = useLoaderData()
+  console.log(menu)
   return <h1>Menu</h1>;
 }
 
