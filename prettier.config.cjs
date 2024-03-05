8d58631172281d956a755b85fcd8016649baa0b0
zeenat <zeenu.23@gmail.com>
2024-03-05 19:51:05 +0530

    Formatted code with Prettier, removed unused CSS and JavaScript files, and updated plugin imports

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,10 +1,4 @@
-// {
-//   "plugins": ["prettier-plugin-tailwindcss"],
-// }
-
-// eslint-disable-next-line no-undef
 module.exports = {
-  // eslint-disable-next-line no-undef
   plugins: [require('prettier-plugin-tailwindcss')],
   singleQuote: true,
 };
