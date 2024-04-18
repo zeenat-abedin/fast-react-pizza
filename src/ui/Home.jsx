6ed5995779542240d67db51926a2d371a98cb248
zeenat <zeenu.23@gmail.com>
2024-04-18 22:01:11 +0530

    update prettier-plugin-tailwindcss to version 0.5.14

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1 className="text-xl  text-stone-700 font-semibold text-center">
+      <h1 className="text-xl font-semibold text-center">
         The best pizza.
         <br />
        <span className="text-yellow-500"> Straight out of the oven, straight to you.</span>
