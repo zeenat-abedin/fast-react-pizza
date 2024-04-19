401f6f55e3df674c45fa8eec3c5aff5300339282
zeenat <zeenu.23@gmail.com>
2024-04-19 16:45:54 +0530

    update Home component layout and styling

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -2,8 +2,8 @@ import CreateUser from "../features/user/CreateUser";
 
 function Home() {
   return (
-    <div>
-      <h1 className="text-xl font-semibold text-center">
+    <div className="mt-10 text-center">
+      <h1 className="mb-8 text-xl font-semibold">
         The best pizza.
         <br />
        <span className="text-yellow-500"> Straight out of the oven, straight to you.</span>
