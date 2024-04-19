73cb0ca26ba5c91d7ff346b94457ae4d9a2da88a
zeenat <zeenu.23@gmail.com>
2024-04-19 20:14:47 +0530

    update background color variables and add spacing in CartOverview, Header, and Home components

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div className="bg-stone-800 p-4 uppercase text-stone-200">
-      <p className="text-stone-300">
+    <div className="bg-stone-800 p-4 uppercase text-stone-200 sm:space-x-6">
+      <p className="space-x-4 text-stone-300 font-semibold sm:space-x-6">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import SearchOrder from "../features/order/SearchOrder"
 
  function Header() {
   return (
-    <header className="border-b border-stone-800 bg-yellow-500 px-4 py-3 uppercase">
+    <header className="border-b border-stone-800 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">Fast React Pizza Co.</Link>
       <SearchOrder/>
       <Username />
