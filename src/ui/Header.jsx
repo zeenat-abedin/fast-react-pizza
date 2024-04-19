617b69c4ce04c5b7a1ec7d69c1f2050c72e9a934
zeenat <zeenu.23@gmail.com>
2024-04-19 19:28:41 +0530

    update components with consistent padding, colors, and text styling

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,8 +4,8 @@ import SearchOrder from "../features/order/SearchOrder"
 
  function Header() {
   return (
-    <header className="bg-yellow-500">
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="border-b border-stone-800 bg-yellow-500 px-4 py-3 uppercase">
+      <Link to="/" className="tracking-widest">Fast React Pizza Co.</Link>
       <SearchOrder/>
       <Username />
     </header>
