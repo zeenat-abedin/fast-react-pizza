be38281cdc2a2e4f60db939bfab7038098536912
zeenat <zeenu.23@gmail.com>
2024-05-02 19:12:24 +0530

    add Redux Toolkit for managing state; update CreateUser and Username components to use Redux; wrap App component in Provider; update package-lock.json

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,13 +1,12 @@
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 function Username() {
-//   const username = useSelector((state) => state.user.username);
+  const username = useSelector((state) => state.user.username);
 
-//   if (!username) return null;
+  if (!username) return null;
 
   return (
-    // <div className="hidden text-sm font-semibold md:block">{username}</div>
-    <div className="hidden text-sm font-semibold md:block">Zeenat</div>
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
   );
 }
 
