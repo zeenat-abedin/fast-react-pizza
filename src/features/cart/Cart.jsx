20192bae2528edd5ffe878731b7047e3ca4dbfe3
zeenat <zeenu.23@gmail.com>
2024-05-03 19:19:53 +0530

    update Cart.jsx to display items in cart using CartItem component; replace fakeCart data with updated version

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,42 +1,49 @@
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import CartItem from './CartItem';
 
-// const fakeCart = [
-//   {
-//     pizzaId: 12,
-//     name: 'Mediterranean',
-//     quantity: 2,
-//     unitPrice: 16,
-//     totalPrice: 32,
-//   },
-//   {
-//     pizzaId: 6,
-//     name: 'Vegetale',
-//     quantity: 1,
-//     unitPrice: 13,
-//     totalPrice: 13,
-//   },
-//   {
-//     pizzaId: 11,
-//     name: 'Spinach and Mushroom',
-//     quantity: 1,
-//     unitPrice: 15,
-//     totalPrice: 15,
-//   },
-// ];
+const fakeCart = [
+  {
+    pizzaId: 12,
+    name: 'Mediterranean',
+    quantity: 2,
+    unitPrice: 16,
+    totalPrice: 32,
+  },
+  {
+    pizzaId: 6,
+    name: 'Vegetale',
+    quantity: 1,
+    unitPrice: 13,
+    totalPrice: 13,
+  },
+  {
+    pizzaId: 11,
+    name: 'Spinach and Mushroom',
+    quantity: 1,
+    unitPrice: 15,
+    totalPrice: 15,
+  },
+];
 
 function Cart() {
   const username = useSelector((state) => state.user.username);
-  // const cart = fakeCart;
+  const cart = fakeCart;
 
   return (
     <div>
       <Link to="/menu">&larr; Back to menu</Link>
 
-      <h2>Your cart, { username }</h2>
+      <h2>Your cart, {username}</h2>
+      
+      <ul className='mt-3 divide-y divide-stone-200 border-b'>
+        {
+          cart.map((item) => <CartItem item={item} key={item.key} />)
+        }
+      </ul>
 
       <div>
-        <Link to="/order/new">Order pizzas</Link>
+        <Link to="/order/new" type='primary'>Order pizzas</Link>
         <button>Clear cart</button>
       </div>
     </div>
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -5,8 +5,8 @@ import Button from "./Button";
 function Home() {
   const username = useSelector((state) => state.user.username);
   return (
-    <div className="my-10 text-center sm:my-16">
-      <h1 className="mb-8 text-xl font-semibold">
+    <div className="my-10 px-4 text-center sm:my-16">
+      <h1 className="mb-8 text-xl font-semibold md:text-3xl">
         The best pizza.
         <br />
        <span className="text-yellow-500"> Straight out of the oven, straight to you.</span>
