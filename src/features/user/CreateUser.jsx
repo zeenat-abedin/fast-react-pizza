9745691382e18f3467699153b04e823b7441c8db
zeenat <zeenu.23@gmail.com>
2024-05-03 19:59:47 +0530

    add margin bottom class and text size class to welcome message in CreateUser form

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -19,7 +19,7 @@ function CreateUser() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <p>👋 Welcome! Please start by telling us your name:</p>
+      <p className='mb-4 text-sm text-stone-600 md:text-base'>👋 Welcome! Please start by telling us your name:</p>
 
       <input
         type="text"
