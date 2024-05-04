7862886ae6a32c81476f2f19ef2fe04de48214a9
zeenat <zeenu.23@gmail.com>
2024-05-04 20:56:29 +0530

    format total cart price with helper function and display using selector

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,15 +1,19 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { formatCurrency } from '../../utils/helpers';
+import { getTotalCartPrice } from "./cartSlice";
 
 function CartOverview() {
   const totalCartQuantity = useSelector((state) => state.cart.cart.reduce((acc, curr) => acc + curr.quantity, 0))
-  if (totalCartQuantity === 0) return null;
+  
+  const totalCartPrice = useSelector(getTotalCartPrice)
 
+  if (!totalCartQuantity) return null;
   return (
     <div className="flex items-center justify-between bg-stone-800 p-4 uppercase text-stone-200 sm:space-x-6">
       <p className="space-x-4 text-stone-300 font-semibold sm:space-x-6">
         <span>{totalCartQuantity} pizzas</span>
-        <span>$23.45</span>
+        <span>{formatCurrency(totalCartPrice)}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
