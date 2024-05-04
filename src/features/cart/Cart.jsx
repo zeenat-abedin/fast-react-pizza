dcfd522bd9d8f41fa501c91f1f4e6c33fea1c34d
zeenat <zeenu.23@gmail.com>
2024-05-04 21:32:18 +0530

    replace fake cart with actual cart data from Redux store; update import statements

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,38 +1,17 @@
-import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import CartItem from './CartItem';
+import { useSelector } from 'react-redux';
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+import LinkButton from '../../ui/LinkButton';
+import CartItem from './CartItem';
+import { getCart } from './cartSlice';
 
 function Cart() {
   const username = useSelector((state) => state.user.username);
-  const cart = fakeCart;
+  const cart = useSelector(getCart);
 
   return (
-    <div>
-      <Link to="/menu">&larr; Back to menu</Link>
+    <div className='px-4 py-3'>
+      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
       <h2>Your cart, {username}</h2>
       
