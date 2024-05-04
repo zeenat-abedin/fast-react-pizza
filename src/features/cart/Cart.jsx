fe07bcaa006773adea55ef0cad7e47228bcd8c3e
zeenat <zeenu.23@gmail.com>
2024-05-04 21:50:24 +0530

    add dispatch and clearCart function in Cart component

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,14 +1,14 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import LinkButton from '../../ui/LinkButton';
 import CartItem from './CartItem';
-import { getCart } from './cartSlice';
+import { clearCart, getCart } from './cartSlice';
 import Button from '../../ui/Button';
 
 function Cart() {
   const username = useSelector((state) => state.user.username);
   const cart = useSelector(getCart);
-
+  const dispatch = useDispatch()
   return (
     <div className='px-4 py-3'>
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
@@ -23,7 +23,7 @@ function Cart() {
 
       <div className='mt-6 space-x-2'>
         <Button to="/order/new" type='primary'>Order pizzas</Button>
-        <Button type="secondary" onClick={()=>{}}>Clear cart</Button>
+        <Button type="secondary" onClick={()=>dispatch(clearCart())}>Clear cart</Button>
       </div>
     </div>
   );
