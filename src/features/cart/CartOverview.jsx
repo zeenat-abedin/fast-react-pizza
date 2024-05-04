4ab48ba6074ce15ad2dc33a95277b0140dcb1258
zeenat <zeenu.23@gmail.com>
2024-05-04 20:34:34 +0530

    add functionality to show cart overview only if there are items in the cart; display total number of pizzas in the cart

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,10 +1,14 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 function CartOverview() {
+  const totalCartQuantity = useSelector((state) => state.cart.cart.reduce((acc, curr) => acc + curr.quantity, 0))
+  if (totalCartQuantity === 0) return null;
+
   return (
-    <div className="bg-stone-800 p-4 uppercase text-stone-200 sm:space-x-6">
+    <div className="flex items-center justify-between bg-stone-800 p-4 uppercase text-stone-200 sm:space-x-6">
       <p className="space-x-4 text-stone-300 font-semibold sm:space-x-6">
-        <span>23 pizzas</span>
+        <span>{totalCartQuantity} pizzas</span>
         <span>$23.45</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
